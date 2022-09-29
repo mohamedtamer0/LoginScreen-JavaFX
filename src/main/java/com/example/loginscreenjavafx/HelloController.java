4ec7ae5fe1c921d3f421de877c5b2adfa256ecfc
mohamed.tamer <mt4452242@gmail.com>
2022-09-29 12:15:26 +0200

    update

--- src/main/java/com/example/loginscreenjavafx/HelloController.java
+++ src/main/java/com/example/loginscreenjavafx/HelloController.java
@@ -4,22 +4,34 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class HelloController {
 
     @FXML
     private Button btnCancel;
-
     @FXML
     private Label lblMSG;
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private PasswordField txtPassword;
+
 
     public void btnCancelOnAction(ActionEvent e ){
         Stage stage = (Stage) btnCancel.getScene().getWindow();
         stage.close();
     }
 
-    public void lblMSGOnAction(ActionEvent e ){
-        lblMSG.setText("You try to Login");
+    public void btnLoginOnAction(ActionEvent e ){
+
+        if (txtUserName.getText().isBlank() == false && txtPassword.getText().isBlank() == false) {
+            lblMSG.setText("You try to Login");
+        }else {
+            lblMSG.setText("Please enter username and password.");
+        }
     }
+
 }
